15f94746559d9986c2ca44607bc34907415c9fe9
IONUT LUCIAN CIUNTUC <41718857+ionuting@users.noreply.github.com>
2026-02-12 15:50:32 +0200

    Add files via upload

--- src/qto_buccaneer/colab_viewer/__init__.py
+++ src/qto_buccaneer/colab_viewer/__init__.py
@@ -1,6 +1,19 @@
-# Colab Viewer Configuration Files
-# 
-# This folder contains default configuration files for the IFC viewer.
-#
-# Files:
-#   - default_colors.yaml: Default color mappings for IFC element types
+"""
+IFC Viewer Module for qto_buccaneer
+Interactive 3D visualization of IFC models in Jupyter/Colab notebooks.
+"""
+
+from .ifc_viewer import visualize_ifc
+from .ifc_viewer_loader import IFCDownloader
+from .ifc_viewer_geometry import GeometryExtractor
+from .ifc_viewer_hierarchy import HierarchicalStructure
+from .ifc_viewer_visualizer import Visualizer3D
+
+__all__ = [
+    'visualize_ifc',
+    'IFCDownloader',
+    'GeometryExtractor',
+    'HierarchicalStructure',
+    'Visualizer3D'
+]
+
